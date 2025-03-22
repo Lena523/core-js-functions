4febc5707e73bd28d54d8920b12bc0e4ebb33784
Elena Khadasevich <camatsu90@mail.ru>
2025-03-22 15:53:22 +0300

    feat: add getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -54,8 +54,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const args = funcs;
+  const iterator = [...args.keys()];
+  return iterator;
 }
 
 /**
